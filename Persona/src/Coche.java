f2b78d90ada5df4dd0954c57f6fd866ed1806215
Andrés <andres.saludambiental@gmail.com>
2020-11-30 19:06:36 +0100

    Cambio en coche

--- Persona/src/Coche.java
+++ Persona/src/Coche.java
@@ -2,11 +2,13 @@
 public class Coche {
 
 	private String nombre;
-	
+	private int matricula;
 	Coche(){
 		nombre=null;
+		matricula=0;
 	}
-	Coche(String nombre){
+	Coche(String nombre, int matricula){
 		this.nombre=nombre;
+		this.matricula=matricula;
 	}
 }
